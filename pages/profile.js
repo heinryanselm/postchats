cc50efeadb0e8dcc41f4831b93edebc4a10a58af
Heinry-Anselm <50972784+anselm-chilz@users.noreply.github.com>
2024-05-27 14:37:15 +0100

    Added edits

--- pages/profile.js
+++ pages/profile.js
@@ -93,7 +93,7 @@ export default function ProfilePage() {
                   )}
                   {!editMode && (
                     <div className="text-gray-500 leading-4">
-                      {profile?.place || 'Internet'}
+                      {profile?.place || 'United Kingdom'}
                     </div>
                   )}
                   {editMode && (
